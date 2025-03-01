831004f16c2925f8b37c08af16d25644e3aecc4b
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-03-01 12:47:18 +0100

    high and low

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <script setup>
 import { ref } from "vue";
 
-const msg = ref("Shopping List");
+const header = ref("Shopping List");
 const items = ref([
   { id: 1, label: "10 party hats" },
   { id: 2, label: "2 board games" },
   { id: 3, label: "20 cups" },
 ]);
+const priority = ref("high");
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-  <ul v-for="item in items" :key="item.id">
-    {{
-      item.label
-    }}
+  <h1>{{ header }}</h1>
+  <input type="text" />
+  <button>Add</button>
+<label for="priority">
+  High:
+  <input v-model="priority" type="radio">
+</label>
+<label for="">
+  Low:
+  <input v-model="priority" type="radio">
+</label>
+
+  <ul v-for="({id, label}, index) in items" :key="id">
+    <li>{{ label }}</li>
   </ul>
 </template>
