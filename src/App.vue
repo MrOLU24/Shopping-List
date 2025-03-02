900191b0be0dcf3d13f80c7c7871175214c91c51
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-03-02 23:03:25 +0100

    toggling the btn

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,13 @@ import { ref } from "vue";
 
 // Reactive reference for the list header
 const header = ref("Shopping List");
+const editing = ref(false);
 
 // Array holding shopping list items, each item has an id and label
 const items = ref([
-  { id: 1, label: "10 party hats" },
-  { id: 2, label: "2 board games" },
-  { id: 3, label: "20 cups" },
+  // { id: 1, label: "10 party hats" },
+  // { id: 2, label: "2 board games" },
+  // { id: 3, label: "20 cups" },
 ]);
 
 // Ref for new item input
@@ -23,22 +24,30 @@ const saveItems = () => {
   // if (!newItem.value.trim()) return;
 
   // Add the new item to the list with a unique ID
-  items.value.push({ 
-    id: Date.now(), // Ensures a unique ID 
-    label: newItem.value 
+  items.value.push({
+    id: Date.now(), // Ensures a unique ID
+    label: newItem.value,
   });
 
   // Clear input after adding
+  newItem.value = "";
+};
+const doEdit = (e) => {
+  editing.value = e;
   newItem.value = "";
 };
 </script>
 
 <template>
   <!-- Page Header -->
-  <h1>{{ header }}</h1>
+  <div class="header">
+    <h1>{{ header }}</h1>
+    <button class="btn" @click="doEdit(false)">Cancel</button>
+    <button class="btn btn-primary" @click="doEdit(true)">Add Items</button>
+  </div>
 
   <!-- Form to Add New Items -->
-  <form class="add-item-form" @submit.prevent="saveItems">
+  <form v-if="editing" class="add-item-form" @submit.prevent="saveItems">
     <!-- Input for new items -->
     <input type="text" v-model.trim="newItem" placeholder="add new items" />
 
@@ -58,4 +67,5 @@ const saveItems = () => {
       {{ label }}
     </li>
   </ul>
+  <p v-if="!items.length">Nothing to show</p>
 </template>
