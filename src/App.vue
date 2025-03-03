9fff5293374ee48713b4754e7f7348b69d74b8d2
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-03-03 09:30:02 +0100

    computed prop

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <script setup>
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 // Reactive reference for the list header
 const header = ref("Shopping List");
 const editing = ref(false);
-
+const characterCount = computed(() => {
+  return newItem.value.length;
+});
 // Array holding shopping list items, each item has an id and label
 const items = ref([
   { id: 1, label: "10 party hats", purchased: true, highPriority: false },
   { id: 2, label: "2 board games", purchased: true, highPriority: false },
   { id: 3, label: "20 cups", purchased: false, highPriority: true },
 ]);
-
 // Ref for new item input
 const newItem = ref("");
-
 // Boolean ref to mark high-priority items (currently not used)
 const newHighPriority = ref(false);
 
@@ -27,17 +27,17 @@ const saveItems = () => {
   items.value.push({
     id: Date.now(), // Ensures a unique ID
     label: newItem.value,
-    highPriority: newHighPriority.value
+    highPriority: newHighPriority.value,
   });
 
   // Clear input after adding
   newItem.value = "";
-  newHighPriority.value = ""
+  newHighPriority.value = "";
 };
 const doEdit = (e) => {
   editing.value = e;
   newItem.value = "";
-  newHighPriority
+  newHighPriority;
 };
 
 const togglePurchased = (item) => {
@@ -67,6 +67,7 @@ const togglePurchased = (item) => {
     <!-- Button to submit the new item -->
     <button class="btn btn-primary">Save Items</button>
   </form>
+  <p>{{ characterCount }}/20</p>
 
   <!-- List of Items -->
   <ul>
@@ -75,7 +76,7 @@ const togglePurchased = (item) => {
       v-for="(items, index) in items"
       :key="items.id"
       class="static-class"
-      :class="{ strikeout: items.purchased, priority: items.highPriority}"
+      :class="{ strikeout: items.purchased, priority: items.highPriority }"
     >
       {{ items.label }}
     </li>
